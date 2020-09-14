23297fc930e3e23246dbc0d96d53828395c05348
Alex Lin <alexlin247@gmail.com>
2020-09-13 23:33:45 -0400

    Add InfoMenuPage

---
+++ DCCovidConnect/DCCovidConnect/Views/InfoMenuPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DCCovidConnect.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class InfoMenuPage : ContentPage
+    {
+        public InfoMenuPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+
+            PageLayout.WidthRequest = 3.0 / 5 * InfoMenu.Height;
+        }
+    }
+}

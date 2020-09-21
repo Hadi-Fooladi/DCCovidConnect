3a08779b2a187d3a6f2c6cef1328d7325edaf9ae
Alex Lin <alexlin247@gmail.com>
2020-09-21 14:42:57 -0400

    Add InfoDetailPage and InfoListPage

---
+++ DCCovidConnect/DCCovidConnect/Views/InfoDetailPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DCCovidConnect.Views
+{
+    public partial class InfoDetailPage : ContentPage
+    {
+        public InfoDetailPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DCCovidConnect/DCCovidConnect/Views/InfoListPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using DCCovidConnect.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DCCovidConnect.Views
+{
+    public partial class InfoListPage : ContentPage
+    {
+        public InfoListPage()
+        {
+            InitializeComponent();
+            BindingContext = new InfoListViewModel();
+        }
+    }
+}

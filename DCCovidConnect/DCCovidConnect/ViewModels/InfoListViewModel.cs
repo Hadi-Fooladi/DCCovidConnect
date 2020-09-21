cb117ec2a3dc99a717a11dacf94898b118f18f77
Alex Lin <alexlin247@gmail.com>
2020-09-21 14:43:51 -0400

    Add BaseViewModel and viewmodel for InfoListPage

---
+++ DCCovidConnect/DCCovidConnect/ViewModels/InfoListViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace DCCovidConnect.ViewModels
+{
+    [QueryProperty("Section", "section")]
+    class InfoListViewModel : BaseViewModel
+    {
+        string section;
+        public string Section
+        {
+            get => section;
+            set => SetProperty(ref section, Uri.UnescapeDataString(value));
+        }
+    }
+}

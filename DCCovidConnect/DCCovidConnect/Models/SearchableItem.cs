739492276a2cf91fd7b8c4c86d65dd8d0552950e
Alex Lin <alexlin247@gmail.com>
2021-04-18 15:13:20 -0400

    Save searchable queries to database

---
+++ DCCovidConnect/DCCovidConnect/Models/SearchableItem.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+
+namespace DCCovidConnect.Models
+{
+    public class SearchableItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public int Priority { get; set; }
+    }
+}

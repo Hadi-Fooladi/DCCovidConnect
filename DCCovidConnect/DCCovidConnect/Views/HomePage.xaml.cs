e5d8bc7e58948b04497af45139842e9ea8ffdf44
Alex Lin <alexlin247@gmail.com>
2021-04-18 15:14:36 -0400

    Update Home Page layout

--- DCCovidConnect/DCCovidConnect/Views/HomePage.xaml.cs
+++ DCCovidConnect/DCCovidConnect/Views/HomePage.xaml.cs
@@ -7,17 +7,75 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DCCovidConnect.Services;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace DCCovidConnect.Views
 {
     public partial class HomePage : ContentPage
     {
+        private HomeViewModel _viewModel;
+        private SKPath _mainState;
+
         public HomePage()
         {
             InitializeComponent();
             BindingContext = new HomeViewModel();
+            _viewModel = BindingContext as HomeViewModel;
+        }
 
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.UpdateVariables();
+            _mainState = MapService.Service.States[Settings.Current.DefaultState].Path;
+            ZoomPath(_mainState);
+        }
+
+        // Values for the canvas camera
+        private readonly float _SCALE_MULTIPLIER = 3.0f;
+        private float _x;
+        private float _y;
+        private float _scale = 4f;
+        
+        private SKMatrix _canvasTranslateMatrix;
+        void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs args)
+        {
+            SKImageInfo info = args.Info;
+            SKSurface surface = args.Surface;
+            SKCanvas canvas = surface.Canvas;
+
+            canvas.Clear();
+
+            _canvasTranslateMatrix = canvas.TotalMatrix;
+            canvas.Scale(_scale);
+
+            SKRect localBounds;
+            canvas.GetLocalClipBounds(out localBounds);
+            float transformedX = _x / canvas.TotalMatrix.ScaleX + localBounds.MidX;
+            float transformedY = _y / canvas.TotalMatrix.ScaleY + localBounds.MidY;
+            canvas.Translate(transformedX, transformedY);
+            
+            SKPaint stateFillPaint = new SKPaint
+            {
+                Style = SKPaintStyle.Fill,
+                Color = ((Color) Application.Current.Resources["Primary"]).ToSKColor()
+            };
+            
+            canvas.DrawPath(_mainState, stateFillPaint);
+        }
+        
+        private bool ZoomPath(SKPath path)
+        {
+            if (path == null || !path.GetBounds(out var targetState))
+                return false;
+            // finds the scale to fit the state to the screen
+            _scale = (float)_canvasView.Height / Math.Max(targetState.Width, targetState.Height) * _SCALE_MULTIPLIER;
+            _x = -targetState.Left * _scale;
+            _y = -targetState.MidY * _scale;
+            _canvasView.InvalidateSurface();
+            return true;
         }
     }
 }

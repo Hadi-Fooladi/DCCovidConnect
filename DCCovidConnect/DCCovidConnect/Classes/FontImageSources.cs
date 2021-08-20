ab950d39156507c14ac6390b70aff333bf44aed0
Hadi Fooladi Talari <hadi.f7@gmail.com>
2021-08-20 16:39:39 -0400

    Code Improvement

---
+++ DCCovidConnect/DCCovidConnect/Classes/FontImageSources.cs
@@ -0,0 +1,23 @@
+﻿using Xamarin.Forms;
+
+namespace DCCovidConnect
+{
+    internal class DefaultFontImageSource : FontImageSource
+    {
+        private static readonly double SIZE = Device.GetNamedSize(NamedSize.Large, typeof(Label));
+
+        public DefaultFontImageSource()
+        {
+            Size = SIZE;
+            FontFamily = "FontAwesomeSolid";
+        }
+    }
+
+    internal class InfoFontImageSource : DefaultFontImageSource
+    {
+        public InfoFontImageSource()
+        {
+            SetDynamicResource(ColorProperty, "AccentColor");
+        }
+    }
+}
